06fe94ef2fd97c0a357d066b69d0277dac82afb2
Edgar Lee <edgarl@netflix.com>
2020-04-11 20:57:46 -0700

    Avoid pdeathsig in jailed environments

--- allocate_linux.go
+++ allocate_linux.go
@@ -5,12 +5,30 @@ package chromedp
 import (
 	"os/exec"
 	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 func allocateCmdOptions(cmd *exec.Cmd) {
 	if cmd.SysProcAttr == nil {
 		cmd.SysProcAttr = new(syscall.SysProcAttr)
 	}
-	// When the parent process dies (Go), kill the child as well.
-	cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
+
+	if !isJailed() {
+		// When the parent process dies (Go), kill the child as well.
+		cmd.SysProcAttr.Pdeathsig = syscall.SIGKILL
+	}
+}
+
+func isJailed() bool {
+	var sig int
+
+	err := unix.Prctl(unix.PR_GET_PDEATHSIG, uintptr(unsafe.Pointer(&sig)), 0, 0, 0)
+	if err != nil {
+		return err.Error() == "operation not permitted"
+	}
+
+	return false
+
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,5 @@ require (
 	github.com/gobwas/pool v0.2.0 // indirect
 	github.com/gobwas/ws v1.0.3
 	github.com/mailru/easyjson v0.7.0
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42
 )
